9ab7507625a444c45dcc0009cad68f8381cb682e
Alexandr Ovsyannikov <alexov2004@gmail.com>
2022-08-16 01:52:53 +0300

    final commit, endpoints created

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-import machinetranslation
-from flask import Flask, render_template
+import machinetranslation.translator as translate
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
@@ -8,6 +8,18 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def return_index():
     return render_template('index.html')
+
+
+@app.route('/englishToFrench')
+def en_to_fr():
+    text = request.args.get('text')
+    return translate.english_to_french(text)
+
+
+@app.route('/frenchToEnglish')
+def fr_to_en():
+    text = request.args.get('text')
+    return translate.french_to_english(text)
 
 
 if __name__ == '__main__':
